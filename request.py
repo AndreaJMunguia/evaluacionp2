f2bd39780a7cd35919e36962ba3e31920e97c43a
AndreaJMunguia <108305407+AndreaJMunguia@users.noreply.github.com>
2022-07-05 22:31:38 -0500

    first commit

--- request.py
+++ request.py
@@ -1,7 +1,7 @@
 import json
 import requests
 api_url = "http://localhost:8080/apiv1/employee/addemployee"
-with open("prueba.json", "r") as file:
+with open("empleados.json", "r") as file:
     data = json.load(file)
 todo = data
 response = requests.post(api_url, json=todo)
--- txt-to-json.py
+++ txt-to-json.py
@@ -1,15 +1,15 @@
 import json
 
 filename = open("empleados.txt")
-diccionario = []
+lista = []
 
 for line in filename:
-    pipe_split = {ele.strip() for ele in line.split(",")}
-    line_dict = dict()
-    for ele in pipe_split:
-        key, val = ele.split(":", 1)
-        line_dict[key] = val
-    diccionario.append(line_dict)
+    split = {i.strip() for i in line.split(",")}
+    line = dict()
+    for i in split:
+        key, valor = i.split(":", 1)
+        line[key] = valor
+    lista.append(line)
 
 with open("empleados.json", "w") as f:
-    json.dump(diccionario, f, indent=4)
+    json.dump(lista, f, indent=4)
