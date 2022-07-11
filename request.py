dee64c5264a97624c527983823cf1345e3a972a0
Andrea Juárez Munguia <108305407+AndreaJMunguia@users.noreply.github.com>
2022-07-11 17:48:29 -0500

    send data via post

--- request.py
+++ request.py
@@ -1,9 +1,48 @@
 import json
 import requests
-api_url = "http://localhost:8080/apiv1/employee/addemployee"
-with open("empleados.json", "r") as file:
-    data = json.load(file)
-todo = data
-response = requests.post(api_url, json=todo)
-print(response)
-response.json()
+try:
+    api_url = "http://localhost:8080/apiv1/employee/addemployee"
+    #with open("empleados.json", "r") as file:
+     #   data = json.load(file)
+    diccionario1 = {
+                        "firstname" : "Lucho",
+                        "surname" : "Dominguez",
+                        "country" : 
+                            {	"code" : "mx",
+                                "name" : "Mexico",
+                                "airports" : [
+                                        {"name" : "Aeropuerto Internacional de Toluca"}
+                                    ]
+                            },
+                        "languages" : [
+                            {
+                                "code" : "es",
+                                "name" : "espanol"
+                            }
+                        ]
+                    }
+    diccionario2 = {
+                        "firstname" : "Jean",
+                        "surname" : "Paul Gualtier",
+                        "country" : 
+                            {	"code" : "fr",
+                                "name" : "Francia",
+                                "airports" : [
+                                        {"name" : "Aeropuerto Internacional de la Ciudad de México"}
+                                    ]
+                            },
+                        "languages" : [
+                            {
+                                "code" : "fr",
+                                "name" : "frances"
+                            }
+                        ]
+                    }
+except Exception as error:
+    print(error)
+else:
+    response1 = requests.post(api_url, json = diccionario1)
+    response2 = requests.post(api_url, json = diccionario2)
+    print(str(response1) + str(response2))
+    response1.json()
+    response2.json()
