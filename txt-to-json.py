18246195edbd991a3edbccb0f9c5458eb7562494
Andrea Juárez Munguia <108305407+AndreaJMunguia@users.noreply.github.com>
2022-07-11 17:46:25 -0500

    txt to json conversion

--- txt-to-json.py
+++ txt-to-json.py
@@ -1,15 +1,15 @@
 import json
 
-filename = open("empleados.txt")
-lista = []
+#Convertir el txt a lista
+filename = "empleados.txt"
+with open(filename) as file:
+    for line in file:
+        data = list(line.strip().split(None, 4))
+        print(data)
 
-for line in filename:
-    split = {i.strip() for i in line.split(",")}
-    line = dict()
-    for i in split:
-        key, valor = i.split(":", 1)
-        line[key] = valor
-    lista.append(line)
+description = ['firstname', 'surname', 'country', 'language', 'airport']
 
-with open("empleados.json", "w") as f:
-    json.dump(lista, f, indent=4)
+#Fusionar ambas listas en un diccionario (lista del txt y lista de las descripciones)
+outputData = {description[i]:data[i] for i in range(len(data))}
+with open('formato_resultante.json', 'a') as file2:
+    json.dump(outputData, file2)
